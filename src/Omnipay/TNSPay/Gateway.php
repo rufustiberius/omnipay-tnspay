b87fd96c40f7a4d5b9ee152789386431cd3272ce
Richard Quadling <RQuadling@GMail.com>
2014-04-29 12:50:38 +0100

    Gateway class for TNSPay Driver.

---
+++ src/Omnipay/TNSPay/Gateway.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Omnipay\TNSPay;
+
+use Omnipay\TNSPay\Message\CompletePurchaseRequest;
+use Omnipay\TNSPay\Message\PurchaseRequest;
+use Omnipay\Common\AbstractGateway;
+
+/**
+ * TNSPay Gateway
+ *
+ * @link https://www.datacash.com/developersarea.php
+ */
+class Gateway extends AbstractGateway
+{
+    public function getName()
+    {
+        return 'TNSPay';
+    }
+
+    public function getDefaultParameters()
+    {
+        return array(
+            'merchantId' => '',
+            'password' => '',
+            'testMode' => false
+        );
+    }
+
+    public function getMerchantId()
+    {
+        return $this->getParameter('merchantId');
+    }
+
+    public function setMerchantId($value)
+    {
+        return $this->setParameter('merchantId', $value);
+    }
+
+    public function getPassword()
+    {
+        return $this->getParameter('password');
+    }
+
+    public function setPassword($value)
+    {
+        return $this->setParameter('password', $value);
+    }
+
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\TNSPay\Message\PurchaseRequest', $parameters);
+    }
+
+    public function completePurchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\TNSPay\Message\CompletePurchaseRequest', $parameters);
+    }
+}
